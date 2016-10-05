123d5ee5dbf47b1d69c9c5c7b9b30b441fbc2612
igotyooo <overmars29@gmail.com>
2016-10-05 05:17:51 +0000

    change cache path

--- setgpath.lua
+++ setgpath.lua
@@ -3,5 +3,5 @@ gpath.db.ucf101 = '/home/dgyoo/workspace/datain/UCF101FRAME/'
 gpath.db.stock = '/home/dgyoo/workspace/datain/STOCK/'
 gpath.net.alex_caffe_model = '/home/dgyoo/workspace/net/caffe/bvlc_alexnet.caffemodel'
 gpath.net.alex_caffe_proto = '/home/dgyoo/workspace/net/caffe/bvlc_alexnet_deploy.prototxt'
-gpath.dataout = '/home/dgyoo/workspace/dataout/video-representation'
+gpath.dataout = '/home/dgyoo/workspace/dataout/video-representation-cvpr'
 return gpath
